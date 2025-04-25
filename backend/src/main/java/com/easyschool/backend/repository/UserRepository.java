5e7cd1aa32b22d7e02fbc75634854b6471df66a0
MMattia13 <marianomattia14@gmail.com>
2025-04-25 18:41:32 +0200

    update repository

--- backend/src/main/java/com/easyschool/backend/repository/UserRepository.java
+++ backend/src/main/java/com/easyschool/backend/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.easyschool.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.easyschool.backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {    
+    Optional<User> findByCredentialsEmail(String email);
 }
